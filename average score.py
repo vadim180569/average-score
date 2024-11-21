22a91839e57aa87277f4d2adff74960bc5f4eb89
vadim180569 <vadim9771505001@gmail.com>
2024-11-21 16:22:28 +0300

    Update average score.py

--- average score.py
+++ average score.py
@@ -1,11 +1,4 @@
-grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2,],[4,4,3],[5,5,5,4,5]]
-student1 = "Aaron:" + str(sum(grades[0])/len(grades[0]))
-student2 = "Bildo:" + str(sum(grades[1])/len(grades[1]))
-student3 = "Johnni:" + str (sum(grades[2])/len(grades[2]))
-student4 = "Khendrik:" + str (sum(grades[3])/len(grades[3]))
-student5 = "Stive:" + str (sum(grades[4])/len(grades[4]))
-students = sorted(list({'Johnni','Bilbo','Stive','Khendrik','Aaron'}))
-print(student1,student2,student3,student4,student5)
+
 
 
 
